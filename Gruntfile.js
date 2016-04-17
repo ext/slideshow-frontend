8b80fe284c7eee382c98a5a58425b0920b2dbb0a
David Sveningsson <ext@sidvind.com>
2016-04-17 12:54:40 +0200

    npm: jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,7 @@ module.exports = function(grunt){
 			libs: {
 				files: {
 					'slideshow/static/js/libs.min.js': [
+						'node_modules/jquery/dist/jquery.js',
 						'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
 					],
 				},
