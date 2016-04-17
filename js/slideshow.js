7fd6719ef6e9373dbe81fc08075f7dd3baf28ab0
David Sveningsson <ext@sidvind.com>
2016-04-17 22:39:45 +0200

    remove jquery-foldable
    not used, replaced with accordion

--- js/slideshow.js
+++ js/slideshow.js
@@ -173,15 +173,4 @@ $(document).ready(function(){
 		timeout: 200,
 	});
 
-	/* fold slide upload fieldsets */
-	var $f = $('.foldable');
-	$f.foldable({
-		collapsed: function(){
-			/* all start collapsed but text assembler */
-			return $(this).attr('id') !== 'assembler_text';
-		},
-		expanded_html: '',
-		collapsed_html: '',
-		connected: $f,
-	});
 });
