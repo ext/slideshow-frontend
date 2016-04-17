94b6c1fe500b73c237ebb1399a2d602cbb687f6b
David Sveningsson <ext@sidvind.com>
2016-04-17 17:18:45 +0200

    js: move text preview to new file

---
+++ js/_textpreview.js
@@ -0,0 +1,38 @@
+/* text preview (see text assembler) */
+(function($){
+	'use strict';
+
+	var delay = 800; /* ms */
+	var fields = [];
+	var timer;
+
+	return $(init);
+
+	function getParams(){
+		var data = {};
+		fields.each(function(){
+			data[$(this).attr('name')] = $(this).val();
+		});
+		return $.param(data);
+	}
+
+	function update(){
+		var url = '/slides/preview?' + getParams();
+		$('img.preview-target').attr('src', url);
+	}
+
+	function init(){
+		fields = $('.assembler.have-preview .trigger-preview').find('input, textarea');
+		fields
+			.blur(update)
+			.keydown(function(){
+				clearTimeout(timer);
+				timer = setTimeout(update, delay);
+			})
+		;
+
+		/* initial preview */
+		update();
+	}
+
+})(jQuery);
