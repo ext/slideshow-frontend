6548c2061c229ba3576d4cbc32eb5461430efd58
David Sveningsson <ext@sidvind.com>
2016-04-18 23:18:08 +0200

    status service

---
+++ js/service/status.service.js
@@ -0,0 +1,44 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('slideshow')
+		.constant('StatusInterval', 10000)
+		.constant('StatusEndpoint', '/api/v1/status')
+		.factory('StatusService', StatusService)
+	;
+
+	StatusService.$inject = ['$interval', '$http', '$rootScope', 'StatusInterval', 'StatusEndpoint'];
+	function StatusService($interval, $http, $rootScope, StatusInterval, StatusEndpoint){
+		var cache = {};
+
+		return {
+			init: init,
+			poll: poll,
+			state: state,
+		};
+
+		function init(){
+			$interval(poll, StatusInterval);
+			poll();
+		}
+
+		function poll(){
+			$http.get(StatusEndpoint).then(function(response){
+				var state = response.data.state;
+
+				if ( angular.isUndefined(cache) || cache.value !== state.value ){
+					$rootScope.$broadcast('state:change', state);
+				}
+
+				cache = state;
+			});
+		}
+
+		function state(){
+			return cache.value;
+		}
+
+	}
+
+})();
